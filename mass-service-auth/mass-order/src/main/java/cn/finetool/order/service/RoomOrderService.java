11a4ee4040ed43033a1a3586b9ffae16bdfb29e7
胡黄林 <huanglin.hu@finetool.cn>
2024-08-13 15:18:48 +0800

    []<fix>: 完善了 创建房间预定订单 功能

--- mass-service-auth/mass-order/src/main/java/cn/finetool/order/service/RoomOrderService.java
+++ mass-service-auth/mass-order/src/main/java/cn/finetool/order/service/RoomOrderService.java
@@ -1,9 +1,10 @@
 package cn.finetool.order.service;
 
+import cn.finetool.common.dto.RoomBookingDto;
 import cn.finetool.common.po.RoomOrder;
 import cn.finetool.common.util.Response;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 public interface RoomOrderService extends IService<RoomOrder> {
-    Response createRoomOrder(RoomOrder roomOrder);
+    Response createRoomOrder(RoomBookingDto roomBookingDto);
 }
