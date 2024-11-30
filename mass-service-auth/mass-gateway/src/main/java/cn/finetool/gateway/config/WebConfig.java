02382d90328568773f4bad8b402520febc333d14
胡黄林 <huhuangl@seeyon.com>
2024-12-01 00:22:47 +0800

    []<feat>: 新增 8000 访问端口

--- mass-service-auth/mass-gateway/src/main/java/cn/finetool/gateway/config/WebConfig.java
+++ mass-service-auth/mass-gateway/src/main/java/cn/finetool/gateway/config/WebConfig.java
@@ -9,11 +9,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 允许跨域请求的路径
-                .allowedOrigins("http://localhost:3000")  // 允许的来源
-                .allowedMethods("GET", "POST", "PUT", "DELETE")  // 允许的请求方法
-                .allowedHeaders("*")  // 允许的请求头
-                .allowCredentials(true)  // 是否发送 Cookies
-                .maxAge(3600);  // 预检请求的缓存时间
+        registry.addMapping("/**")
+                .allowedOriginPatterns("http://localhost:3000", "http://localhost:8000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
